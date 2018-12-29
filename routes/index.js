5a4b9a30fd3dfd5ddcd184d54de58364ece7051a
Aleksandr-41-53 <darkginesys@gmail.com>
2018-12-29 23:35:07 +0300

    add logout backend...

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express   = require('express');
 const router    = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 
 
 // Welcome page
 router.get('/', (req, res) => res.render('welcome'));
 // Dashboard
-router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+    res.render('dashboard', {
+        username: req.user.username
+    }));
 
 module.exports = router;
